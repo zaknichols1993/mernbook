8048ead796bb88f044424ed86898a264192a322b
zaknichols1993 <zaknichols1993@gmail.com>
2018-08-12 12:40:56 -0400

    added jwt payload to user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User Model
 const User = require('../../models/User');
@@ -54,7 +56,20 @@ router.post('/login', (req, res) => {
         // Check Password
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
-                res.json({ msg: 'Success' });
+                const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+                
+                // Sign Token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                    }
+                );
             } else {
                 return res.status(400).json({ password: 'Password Incorrect' });
             }
