e2837e23c0ef1f55bd15b0cf6e7657cafcb348a4
zaknichols1993 <zaknichols1993@gmail.com>
2018-08-13 16:36:20 -0400

    made a basic layout in react and tested a few components

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <footer className="text-center">
+            Copyright &copy; {new Date().getFullYear()} MERNbook
+        </footer>
+    );
+};
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="text-center">landing</div>
+        );
+    }
+}
+
+export default Landing;
