5316f85624cf481d2dcbf59b6d996d6d403d3d62
zaknichols1993 <zaknichols1993@gmail.com>
2018-08-14 11:35:20 -0400

    added react router. Added a few client side routes to test

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default () => {
     return (
-        <footer className="text-center">
-            Copyright &copy; {new Date().getFullYear()} MERNbook
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} DevConnector
         </footer>
     );
 };
