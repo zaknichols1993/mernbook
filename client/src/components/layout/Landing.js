18d23b0c72b690dc8950fe43afea4d8068db6ae2
zaknichols1993 <zaknichols1993@gmail.com>
2018-08-16 10:41:07 -0400

    added axios package for http requests. Tested register route in console and everything working thus far

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,11 +9,11 @@ class Landing extends Component {
               <div className="container">
                 <div className="row">
                   <div className="col-md-12 text-center">
-                    <h1 className="display-3 mb-4">Developer Connector</h1>
+                    <h1 className="display-3 mb-4">MERNbook</h1>
                     <p className="lead">
                       {' '}
-                      Create a developer profile/portfolio, share posts and get help
-                      from other developers
+                      Create a profile/portfolio, share posts and get help
+                      from other's
                     </p>
                     <hr />
                     <Link to="/register" className="btn btn-lg btn-info mr-2">
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
             <div className="container">
               <Link className="navbar-brand" to="/">
-                DevConnector
+                MERNbook
               </Link>
               <button
                 className="navbar-toggler"
@@ -22,7 +22,7 @@ class Navbar extends Component {
                   <li className="nav-item">
                     <Link className="nav-link" to="/profiles">
                       {' '}
-                      Developers
+                      Profiles
                     </Link>
                   </li>
                 </ul>
@@ -40,7 +40,8 @@ class Navbar extends Component {
                 </ul>
               </div>
             </div>
-          </nav>        );
+          </nav>        
+        );
     }
 }
 
